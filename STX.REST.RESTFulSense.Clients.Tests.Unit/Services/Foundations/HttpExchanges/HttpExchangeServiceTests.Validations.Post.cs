15c9b31161a1ac1501f21df22babe4e2711ef747
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2024-10-27 19:13:30 +0000

    ShouldThrowHttpExchangeValidationExceptionOnPostIfHttpExchangeRequestIsInvalidAsync -> FAIL

--- STX.REST.RESTFulSense.Clients.Tests.Unit/Services/Foundations/HttpExchanges/HttpExchangeServiceTests.Validations.Post.cs
+++ STX.REST.RESTFulSense.Clients.Tests.Unit/Services/Foundations/HttpExchanges/HttpExchangeServiceTests.Validations.Post.cs
@@ -72,5 +72,71 @@ namespace STX.REST.RESTFulSense.Clients.Tests.Unit.Services.Foundations.HttpExch
 
             this.httpBroker.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null, null, "GET", "0.1", InvalidVersionPolicy)]
+        [InlineData("", "", "GET", "0.1", InvalidVersionPolicy)]
+        [InlineData(" ", " ", "GET", "0.1", InvalidVersionPolicy)]
+        private async Task ShouldThrowHttpExchangeValidationExceptionOnPostIfHttpExchangeRequestIsInvalidAsync(
+            string invalidBaseAddress,
+            string invalidRelativeUrl,
+            string invalidHttpMethod,
+            string invalidHttpVersion,
+            int invalidHttpVersionPolicy)
+        {
+            // given
+            var httpExchange = new HttpExchange()
+            {
+                Request = new HttpExchangeRequest
+                {
+                    BaseAddress = invalidBaseAddress,
+                    RelativeUrl = invalidRelativeUrl,
+                    HttpMethod = invalidHttpMethod,
+                    Version = invalidHttpVersion,
+                    VersionPolicy = invalidHttpVersionPolicy
+                }
+            };
+
+            var invalidHttpExchangeRequestException =
+                new InvalidHttpExchangeRequestException(
+                    message: "Invalid request, fix errors and try again.");
+
+            invalidHttpExchangeRequestException.UpsertDataList(
+                key: nameof(HttpExchangeRequest.BaseAddress),
+                value: "Value is required");
+
+            invalidHttpExchangeRequestException.UpsertDataList(
+                key: nameof(HttpExchangeRequest.RelativeUrl),
+                value: "Value is required");
+
+            invalidHttpExchangeRequestException.UpsertDataList(
+                key: nameof(HttpExchangeRequest.HttpMethod),
+                value: "HttpMethod is invalid");
+
+            invalidHttpExchangeRequestException.UpsertDataList(
+                key: nameof(HttpExchangeRequest.Version),
+                value: "HttpVersion is invalid");
+
+            invalidHttpExchangeRequestException.UpsertDataList(
+                key: nameof(HttpExchangeRequest.VersionPolicy),
+                value: "HttpVersionPolicy is invalid");
+
+            var expectedHttpExchangeValidationException =
+                new HttpExchangeValidationException(
+                    message: "HttpExchange validation errors occurred, fix errors and try again.",
+                    innerException: invalidHttpExchangeRequestException);
+
+            // when
+            ValueTask<HttpExchange> postAsyncTask = httpExchangeService.PostAsync(httpExchange);
+
+            HttpExchangeValidationException actualHttpExchangeValidationException =
+               await Assert.ThrowsAsync<HttpExchangeValidationException>(postAsyncTask.AsTask);
+
+            // then
+            actualHttpExchangeValidationException.Should().BeEquivalentTo(
+                expectedHttpExchangeValidationException);
+
+            this.httpBroker.VerifyNoOtherCalls();
+        }
     }
 }
